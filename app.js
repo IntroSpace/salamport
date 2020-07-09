41218d13251c89d9ed22c2eb3f3d106b0edbd8b7
IntroSpace <IntroSpace321@gmail.com>
2020-07-09 14:32:37 +0300

    update

--- app.js
+++ app.js
@@ -11,68 +11,68 @@ const jsonParser	= express.json()
 //app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname+"/public"))
 
-app.post('/register', jsonParser,function(req,res) {
-    db
-    	.getUser(req.body.email)
-    	.then(function (results) {
-    		if (results.length == 0) {
-    			user = {
-    				email: req.body.email,
-    				firstname: req.body.firstname,
-    				lastname: req.body.lastname,
-    				token: 12345678
-    			}
-    			db
-    				.addUser(user)
-    				.then(function (results) {
-    					res.json({
-    						err: 0,
-    						message: "Пользователь добавлен: " + results[0]
-    					})
-    				})
-    				.catch(function (err) {
-    					res.json({
-    						err: 1,
-    						message: "Ошибка"
-    					})
-    				})
-    		} else {
-    			res.json({
-    				err: 1,
-    				message: "Пользователь уже есть!"
-    			})
-    		}
-    	})
-});
+// app.post('/register', jsonParser,function(req,res) {
+//     db
+//     	.getUser(req.body.email)
+//     	.then(function (results) {
+//     		if (results.length == 0) {
+//     			user = {
+//     				email: req.body.email,
+//     				firstname: req.body.firstname,
+//     				lastname: req.body.lastname,
+//     				token: 12345678
+//     			}
+//     			db
+//     				.addUser(user)
+//     				.then(function (results) {
+//     					res.json({
+//     						err: 0,
+//     						message: "Пользователь добавлен: " + results[0]
+//     					})
+//     				})
+//     				.catch(function (err) {
+//     					res.json({
+//     						err: 1,
+//     						message: "Ошибка"
+//     					})
+//     				})
+//     		} else {
+//     			res.json({
+//     				err: 1,
+//     				message: "Пользователь уже есть!"
+//     			})
+//     		}
+//     	})
+// });
 
-app.post('/user/login',function (req,res) {
-	if (db.haveToken(req.body.email)) {
-		db
-    		.getUser(req.body.email)
-    		.then(function (results) {
-    			if (results[0].token == req.body.token) {
-    				res.json({
-    					email: req.body.email,
-    					firstname: results[0].firstname,
-    					lastname: results[0].lastname,
-    					token: results[0].token
-    				})
-    			}
-    		})
-	}
-    db
-    	.getUser(req.body.email)
-    	.then(function (results) {
-    		if (results[0].token == req.body.token) {
-    			res.json({
-    				email: req.body.email,
-    				firstname: results[0].firstname,
-    				lastname: results[0].lastname,
-    				token: results[0].token
-    			})
-    		}
-    	})
-});
+// app.post('/user/login',function (req,res) {
+// 	if (db.haveToken(req.body.email)) {
+// 		db
+//     		.getUser(req.body.email)
+//     		.then(function (results) {
+//     			if (results[0].token == req.body.token) {
+//     				res.json({
+//     					email: req.body.email,
+//     					firstname: results[0].firstname,
+//     					lastname: results[0].lastname,
+//     					token: results[0].token
+//     				})
+//     			}
+//     		})
+// 	}
+//     db
+//     	.getUser(req.body.email)
+//     	.then(function (results) {
+//     		if (results[0].token == req.body.token) {
+//     			res.json({
+//     				email: req.body.email,
+//     				firstname: results[0].firstname,
+//     				lastname: results[0].lastname,
+//     				token: results[0].token
+//     			})
+//     		}
+//     	})
+// });
 
 function randomCode() {  
   return Math.floor(
